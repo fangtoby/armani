ed55fd0ccf0761d8b484a016a82fea9b9c194ea7
fangtoby <fangtoby@live.cn>
2015-08-11 15:02:57 +0800

    weibo auth

--- protected/controllers/IndexController.php
+++ protected/controllers/IndexController.php
@@ -14,24 +14,11 @@ class IndexController extends Controller
 			$user = User::model()->findByAttributes(array(
 				'unionid'=>$info['openid']
 			));
-			/*
-			$user = User::model()->findAllByAttributes(array(
-				'unionid'=>$info['openid']
-			));*/
-			
 			if(!$user){
 				$user = new User();
 				$user->unionid = $info['openid'];
-				
-				//$tmpStr = json_encode($info['nickname']); //暴露出unicode 
-				//$tmpStr = preg_replace("#(\\\ue[0-9a-f]{3})#ie","addslashes('\\1')",$tmpStr); //将emoji的unicode留下，其他不动 
-				//$nickname = json_decode($tmpStr); 
-				//$user->nickname = $nickname;
-				//$nickname = emoji_html_to_unified($info['nickname']);
 				$nickname = json_encode($info['nickname']);
 				$user->nickname = $nickname;
-				//$text = preg_replace("#\\\u([0-9a-f]+)#ie","iconv('UCS-2','UTF-8', pack('H4', '\\1'))",$text); //对emoji unicode进行二进制pack并转utf8 
-				
 				$user->sex = $info['sex'];
 				$user->headimgurl = $info['headimgurl'];
 				$user->save();
@@ -50,12 +37,6 @@ class IndexController extends Controller
 					'headimgurl'=>$user->headimgurl,
 				)
 			));
-			/*
-			$this->render('list',array(
-				'result'=>$this->result,
-				'url'=>Yii::app()->params['severUrl'],
-				'info'=>$info
-			));*/
 			
 		}else{
 			$id = Yii::app()->params['weichat']['id'];
@@ -72,6 +53,81 @@ class IndexController extends Controller
 			));
 		}
 	}
+	
+	public function actionIndexs()
+	{
+		$path = array(
+			'wx'=>1,
+			'wb'=>2,
+		);
+		
+		$where = 0;
+		if(isset($_GET['code'])){
+			$code = $_GET['code'];
+			echo $code;
+			exit;
+		}
+		if(isset($_GET['path'])){
+			if($path['wx'] == $_GET['path']){
+				$where = 1;
+			}
+			if($path['wb'] == $_GET['path']){
+				$where = 2;
+			}
+		}
+		
+		if(isset($_GET['info']) && $_GET['info'] !=  NULL){
+			$info = CJSON::decode($_GET['info']);
+			
+			$openid = 'oPV4Ht0yokB6n-DEcr5JocRNPZv4';
+			
+			$user = User::model()->findByAttributes(array(
+				'unionid'=>$info['openid']
+			));
+			
+			if(!$user){
+				$user = new User();
+				$user->unionid = $info['openid'];
+				$nickname = json_encode($info['nickname']);
+				$user->nickname = $nickname; 
+				
+				$user->sex = $info['sex'];
+				$user->headimgurl = $info['headimgurl'];
+				$user->save();
+			}
+			
+			Yii::app()->session['uid'] = $user->id;
+			
+			$this->render('list',array(
+				'result'=>$this->result,
+				'url'=>Yii::app()->params['severUrl'],
+				'info'=>array(
+					'openid'=>$user->id,
+					'nickname'=>json_decode($user->nickname),
+					'headimgurl'=>$user->headimgurl,
+				)
+			));
+			
+		}else{
+			$link = Yii::app()->params['severUrl'];
+			if($where == $path['wx']){
+				$id = Yii::app()->params['weichat']['id'];
+				$appId = Yii::app()->params['weichat']['appId'];
+				$domain = Yii::app()->params['weichat']['domain'];
+				$ulink =  urlencode("http://{$domain}/External/Oauth.ashx?link={$link}&id={$id}");
+				$url="https://open.weixin.qq.com/connect/oauth2/authorize?appid={$appId}&redirect_uri={$ulink}&response_type=code&scope=snsapi_userinfo&state=State#wechat_redirect";
+			}else if($where == $path['wb']){
+				$id = "3163304423";
+				$link = $link."/index/indexs";
+				$authorize = "https://api.weibo.com/oauth2/authorize?client_id={$id}&response_type=code&redirect_uri={$link}";
+				$url = urlencode($authorize);
+			}
+			$this->render('index',array(
+				'url'=>$url
+			));
+		}
+	}
+	
 	public function actionList(){
 		if(isset($_GET['test'])){
 			$uid = 5;
