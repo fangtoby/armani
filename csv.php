fde4359e43b58def21156e5b0e91964a712650c3
fangtoby <fangtoby@live.cn>
2015-08-28 20:28:27 +0800

    csv update

--- csv.php
+++ csv.php
@@ -32,6 +32,14 @@ function export_csv($filename,$data) {
     echo $data; 
 } 
 
+$agent = $_SERVER['HTTP_USER_AGENT'];
+
+$isMac = false;
+
+if (eregi('Mac', $agent)){ 
+    $isMac = true;
+}
+
 $sql = 'SELECT c.CityName,m.ShopName,t.phone,p.name,u.nickname,u.sex,t.from,t.createTime FROM lottery t '.
 'left join user u on u.id = t.uid left join market m on m.ShopID = t.marketId left join city c on c.CityID = t.cityId left join prize p on p.id = t.giftId order by t.cityId,t.marketId,t.giftId';
 
@@ -45,8 +53,12 @@ $sexs = array(1=> '男',2=> '女');
 
 $res  = mysql_query($sql);
 
-//$str = "地区,门店,手机号码,奖品名称,客户昵称,性别,来源,抽奖时间\n"; 
 $str = "地区,门店,手机号码,奖品名称,性别,来源,抽奖时间\n"; 
+
+if ($isMac) {
+    $str = "地区,门店,手机号码,奖品名称,客户昵称,性别,来源,抽奖时间\n"; 
+}
+
 
 while ($row = mysql_fetch_assoc($res)) {
 	//var_dump($row);
@@ -55,17 +67,24 @@ while ($row = mysql_fetch_assoc($res)) {
 	$shopname = $row['ShopName'];
 	$phone = $row['phone'];
 	$name = $row['name'];
-	//$nickName = json_decode( $row['nickname'] );
+    if ($isMac) {
+       $nickName = json_decode( $row['nickname'] );
+    }
 	$sex = $sexs[$row['sex']];
 	$from = $fromMap[$row['from']];
 	$createTime = $row['createTime'];
-	//$str .= $cityname.','.$shopname.','.$phone.','.$name.','.$nickName.','.$sex.','.$from.','.$createTime . "\n";
-    $str .= $cityname.','.$shopname.','.$phone.','.$name.','.$sex.','.$from.','.$createTime . "\n";
-    
+	if ($isMac) {
+        $str .= $cityname.','.$shopname.','.$phone.','.$name.','.$nickName.','.$sex.','.$from.','.$createTime . "\n";
+    }else{
+        $str .= $cityname.','.$shopname.','.$phone.','.$name.','.$sex.','.$from.','.$createTime . "\n";
+    }
 }
- $str = iconv('utf-8','gb2312',$str);
- $filename = date('Ymd').'.csv'; //设置文件名 
- export_csv($filename,$str); //导出 
+if (!$isMac) {
+     $str = iconv('utf-8','gb2312',$str);
+}
+
+$filename = date('Ymd').'.csv'; //设置文件名 
+export_csv($filename,$str); //导出 
 
 
 
